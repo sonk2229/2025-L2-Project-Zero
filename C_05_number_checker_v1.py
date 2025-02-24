1d1e9cffea1ff8e02d92916db6d5bcade3cf55a0
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2025-02-24 13:25:29 +1300

    Add files via upload

---
+++ C_05_number_checker_v1.py
@@ -0,0 +1,45 @@
+# Functions go here
+def num_check(question, num_type, exit_code=None):
+        """Checks users enter an integer / float that is more than
+        zero (or the optional exit code)"""
+
+        if num_type == "integer":
+            error = "Oops - Please enter an integer more than zero."
+            change_to = int
+        else:
+            error = "Oops - Please enter an number more than zero."
+            change_to = float
+
+        while True:
+            response = input(question).lower()
+
+            # check for the exit code
+            if response == "xxx":
+                return response
+
+            try:
+                # Change the response to an integer and check that it's more than zero
+                response = int(response)
+
+                if response > 0:
+                    return response
+                else: print(error)
+
+            except ValueError:
+                print(error)
+
+
+# Main Routine goes here
+
+# loop for testing purposes...
+while True:
+    print()
+
+    my_float = num_check("Please enter a number more than 0: ", "float")
+    print()
+    my_int = num_check("Please enter an integer more than 0",
+                       "integer", "")
+    if my_int == "":
+        print("You have chosen infinite mode.")
+    else:
+        print(f"Thanks. You chose {my_int}")
