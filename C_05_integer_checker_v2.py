d86ed5caa6f7bd71f622cac3c44ee1c457e1d2a4
sonk2229 <159740652+sonk2229@users.noreply.github.com>
2025-02-25 14:45:23 +1300

    Add files via upload

---
+++ C_05_integer_checker_v2.py
@@ -0,0 +1,31 @@
+# Functions go here
+def int_check(question, low, high):
+    """Checks users enter an integer between two values"""
+
+    error = f"Oops - Please enter an integer between {low} and {high}."
+
+    while True:
+
+        try:
+            # Change the response to an integer and check that it's more than zero
+            response = int(input(question))
+
+            if low <= response <= high:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Main Routine goes here
+
+# loop for testing purposes...
+while True:
+
+    print()
+
+    # ask user for an integer
+    my_num = int_check("Choose a number: ", 1, 10)
+    print(f"You choose {my_num}")
